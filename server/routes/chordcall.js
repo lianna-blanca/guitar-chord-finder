faddf4e5f693169b2750f430c798b6103eb2c353
lianna-blanca <lianna.blanca@gmail.com>
2018-11-13 19:08:19 +1300

    Add catch err

--- server/routes/chordcall.js
+++ server/routes/chordcall.js
@@ -4,21 +4,17 @@ const router = express.Router()
 const dbFunctions = require("../db/chordDB")
 
 router.get("/", (req,res) => {
-  let chordCalled = "E_maj7" //req.param.chordName
+  let chordCalled = "D_maj7"
+// router.get("/:chord", (req,res) => {
+//   let chordCalled = req.param.chord
 
   dbFunctions.getChord(chordCalled)
   .then(chords => {
-console.log("------------ routes/routeName.js --------")
-console.log(chords)
-    // for (let i = 0; i < chords.length; i++) {
-    //   console.log(chords[i].chordKey)
-      
-    // }
-
-
     res.send(chords)
   })
-
+  .catch(err => {
+    res.status(500).send('DATABASE ERROR: ' + err.message)
+  })
 })
 
 module.exports = router
